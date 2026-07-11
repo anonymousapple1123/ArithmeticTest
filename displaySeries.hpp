9f8ff40a79fc790200970d96bd351495afbbc30f
Ayush Kumar <kumarayush9651@tutanota.com>
2026-07-11 21:59:40 +0530

    added table of msg

--- displaySeries.hpp
+++ displaySeries.hpp
@@ -6,7 +6,17 @@ const int MAX_ITR = 10;
 
 bool mulTables(){
     for(int i = 1; i<=MAX_RANGE; i+=1){
-        for(int j = 1; j<=MAX_RANGE; j+=1){
+        for(int j = 1; j<=MAX_ITR; j+=1){
+            std::cout<<i<<" X "<<j<<"\t=\t"<<i*j<<"\n";
+        }
+        std::cout<<"-----------------------------"<<std::endl;
+    }
+    return true;
+}
+
+bool tableof(int table){
+    for(int i = table; i<=table; i+=1){
+        for(int j = 1; j<=MAX_ITR; j+=1){
             std::cout<<i<<" X "<<j<<"\t=\t"<<i*j<<"\n";
         }
         std::cout<<"-----------------------------"<<std::endl;
