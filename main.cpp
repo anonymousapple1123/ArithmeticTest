d112ae1b5a5cf542c48e5d377474a05b2e54e88f
Ayush Kumar <kumarayush9651@tutanota.com>
2026-07-20 11:18:57 +0530

    implemented start quiz function in the main with most function implemented.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 #include "testMain.hpp"
 
 void printTables();
+void startQuiz();
 int main(){
-    std::cout<<"Main started "<< std::endl;
-    //printTables();
+    int choice{0};
+    std::cout<<"******** MATHEMATICS QUIZ ********"<< std::endl;
+    std::cout<<"1.Print tables. \n2.Start Quiz. \n:";
+    std::cin>>choice;
+    switch (choice) {
+        case 1:
+        printTables();
+        break;
+        case 2:
+        startQuiz();
+        break;
+        default:
+        std::cout<<"Exiting..."<<std::endl;
+    }//switch ended.
 
-    if(askAltMult()){
-        std::cout<<"Correct !\n";
-    }else{ std::cout<<"False ..";}
     return 0;
 }
 
@@ -42,4 +52,50 @@ void printTables(){
         break;
     }
 
+}
+
+//Quiz function implemented here.
+void startQuiz(){
+    int maxques{0}, choice{0};
+    std::cout<<"Provide Number of questions : ";
+    std::cin>>maxques;
+    std::cout<<"Question types :\n";
+    std::cout<<"0.EXIT :\n";
+    std::cout<<"1.Multiple :\n";
+    std::cout<<"2.Squares :\n";
+    std::cout<<"3.Cubes :\n";
+    std::cout<<"4.Alternate Multiple :\n";
+    std::cin>>choice;
+    while(maxques>0){
+        switch(choice){
+            case 0:
+            std::cout<<"Exitting...\n";
+            break;
+            case 1 :
+            if(askMultiple()){
+                std::cout<<"Correct !\n";
+            }else{ std::cout<<"False ..";}
+            break;
+            case 2:
+            if(askSquare()){
+                std::cout<<"Correct !\n";
+            }else{ std::cout<<"False ..";}
+            break;
+            case 3:
+            if(askCube()){
+                std::cout<<"Correct !\n";
+            }else{ std::cout<<"False ..";}
+            break;
+            case 4:
+            if(askAltMult()){
+                std::cout<<"Correct !\n";
+            }else{ std::cout<<"False ..";}
+            break;
+            default:
+            std::cout<<"Exitting...\n";
+            break;
+        }//switch case ends
+        maxques-=1;
+    }//while loop ends
+    return;
 }
